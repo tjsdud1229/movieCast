90b84702f51a3da6c9be3717a8940df55c9b90fa
Tina Kim <tjsdud1229@gmail.com>
2023-04-07 14:11:21 -0700

    test

--- src/index.js
+++ src/index.js
@@ -6,13 +6,20 @@ import store from "./store";
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <HashRouter>
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//     </HashRouter>
+//   </React.StrictMode>,
+//   document.getElementById("app")
+// );
+
 ReactDOM.render(
-  <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </HashRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("app")
 );
